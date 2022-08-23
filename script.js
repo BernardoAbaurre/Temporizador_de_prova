e6e541dd314ed1d2bd612438ad55a10eef9b480f
Bernardo Emerick Abaurre <emerickabaurreb@gmail.com>
2022-08-23 10:32:27 -0300

    tempo por questão

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var modo = hPm
-
+document.getElementById('horas').focus()
 function checar()
 {
     if(document.getElementById('hPm').checked == true)
@@ -7,12 +7,14 @@ function checar()
         document.getElementById('inputH').style.display = "block"
         document.getElementById('inputM').style.display = "none"
         modo = hPm
+        document.getElementById('horas').focus()
     }
     else
     {
         document.getElementById('inputM').style.display = "block"
         document.getElementById('inputH').style.display = "none"
         modo = mPh
+        document.getElementById('minutos').focus()
     }
 }
 
@@ -20,20 +22,79 @@ function converter()
 {
     var inputH = Number(document.getElementById('horas').value)
     var inputM = Number(document.getElementById('minutos').value)
-    var resultadoTXT = document.getElementById('resultadoTXT')
-    var resultado = 0
+    var resultadoConvTXT = document.getElementById('resultadoConvTXT')
+    var resultadoConv = 0
+    var resto = 0 
 
-    resultadoTXT.innerHTML= 'Resultado: '
+    resultadoConvTXT.innerHTML= 'Resultado: '
 
     if(modo == hPm)
     {
-        resultado = inputH * 60
-        resultadoTXT.innerHTML += `${resultado} minutos`
+        resultadoConv = inputH * 60
+        resultadoConvTXT.innerHTML += `${resultadoConv} minutos`
+        document.getElementById('horas').focus()
     }
 
     if(modo == mPh)
     {
-        resultado = Math.floor(inputM/60)
-        resultadoTXT.innerHTML += `${resultado} horas e ${inputM%60} minutos`
+        resto = inputM%60
+        resultadoConv = Math.floor(inputM/60)
+        var palavraH
+        var palavraM
+
+        if(resultadoConv > 1)
+        {
+            palavraH = "horas"
+        }
+        else
+        {
+            palavraH = "hora"
+        }
+
+        if(resto > 1)
+        {
+            palavraM = "minutos"
+        }
+        else
+        {
+            palavraM = "minuto"
+        }
+
+        if (inputM < 60)
+        {
+            resultadoConvTXT.innerHTML += `${inputM} ${palavraM}` 
+        }
+        else if (resto != 0)
+        {
+            resultadoConvTXT.innerHTML += `${resultadoConv} ${palavraH} e ${resto} ${palavraM}`
+        }
+        else
+        {
+            resultadoConvTXT.innerHTML += `${resultadoConv} ${palavraH}`
+        }
+        
+        document.getElementById('minutos').focus()
     }
+}
+function tempoQ()
+{
+    var inputTempo = Number(document.getElementById('tempo').value)
+    var inputQuest = Number(document.getElementById('nQuestoes').value)
+    var resultadoTempoQTXT = document.getElementById('resultadoTempoQTXT')
+    var resultadoTQ
+    var restoTQ
+
+    resultadoTempoQTXT.innerHTML = 'Resultado: '
+
+    resultadoTQ = inputTempo/inputQuest
+    restoTQ = (inputTempo%inputQuest)
+    if(restoTQ != 0)
+    {
+        resultadoTempoQTXT.innerHTML += `${resultadoTQ.toFixed(2)} min por questão`
+    }
+    else
+    {
+        resultadoTempoQTXT.innerHTML += `${resultadoTQ} min por questão`
+    }
+    document.getElementById('tempo').focus()
 }
